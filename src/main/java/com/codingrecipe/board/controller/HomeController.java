efa07db76d016845fa76718d9c25dae0b4359618
codingrecipe1 <codingrecipe1@gmail.com>
2024-02-08 10:58:28 +0900

    02_시작페이지 출력

---
+++ src/main/java/com/codingrecipe/board/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.codingrecipe.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String index() {
+        System.out.println("HomeController.index");
+        return "index";
+    }
+}
