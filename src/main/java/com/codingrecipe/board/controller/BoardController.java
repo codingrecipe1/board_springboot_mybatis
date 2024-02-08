2861cf61309e80b2015a4f1176adc38d13d2647e
codingrecipe1 <codingrecipe1@gmail.com>
2024-02-08 11:16:05 +0900

    04_게시글 작성 화면 구현

---
+++ src/main/java/com/codingrecipe/board/controller/BoardController.java
@@ -0,0 +1,17 @@
+package com.codingrecipe.board.controller;
+
+import com.codingrecipe.board.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class BoardController {
+    private final BoardService boardService;
+
+    @GetMapping("/save")
+    public String save() {
+        return "save";
+    }
+}
