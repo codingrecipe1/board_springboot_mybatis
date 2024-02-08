c58b30e50cfc73fe0632d8cfe2ae86e710d5904b
codingrecipe1 <codingrecipe1@gmail.com>
2024-02-08 11:26:36 +0900

    05_게시글 작성 데이터 전달하기

--- src/main/java/com/codingrecipe/board/controller/BoardController.java
+++ src/main/java/com/codingrecipe/board/controller/BoardController.java
@@ -1,9 +1,12 @@
 package com.codingrecipe.board.controller;
 
+import com.codingrecipe.board.dto.BoardDTO;
 import com.codingrecipe.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -13,5 +16,11 @@ public class BoardController {
     @GetMapping("/save")
     public String save() {
         return "save";
+    }
+
+    @PostMapping("/save")
+    public String save(BoardDTO boardDTO) {
+        System.out.println("boardDTO = " + boardDTO);
+        return "index";
     }
 }
